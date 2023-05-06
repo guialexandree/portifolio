f0af9ab92da1fd7aec8baef5667b32a62db2d8bf
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-05-06 19:40:24 -0300

    feat: adiciona animatedCursor

---
+++ src/components/AnimatedCursor/index.tsx
@@ -0,0 +1,15 @@
+import { default as AnimatedCursorLib } from 'react-animated-cursor'
+
+export const AnimatedCursor: React.FC = () => {
+  return (
+    <AnimatedCursorLib
+      innerSize={8}
+      outerSize={18}
+      innerScale={0.6}
+      outerScale={2.5}
+      color='83, 91, 242'
+      showSystemCursor={true}
+      type={1}
+    />
+  )
+}
