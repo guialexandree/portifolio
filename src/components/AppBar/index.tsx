3b953cdbc4e8dace27f82c7324c9d056ec87c2e7
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-05-06 21:14:30 -0300

    fix: correção de estilo

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -3,10 +3,10 @@ import './styles.scss'
 export const AppBar: React.FC = () => {
   return (
     <ul className='navbar-container'>
-      <li><a href='#home'>Home</a></li>
-      <li><a href='#resumo'>Resumo</a></li>
-      <li><a href='#projetos'>Projetos</a></li>
-      <li><a href='#contato'>Contato</a></li>
+      <li><a href='#home'>home</a></li>
+      <li><a href='#resumo'>resumo</a></li>
+      <li><a href='#projetos'>projetos</a></li>
+      <li><a href='#contato'>contato</a></li>
     </ul>
   )
 }
