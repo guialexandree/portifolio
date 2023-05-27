02c660386542dd1f7de77ce85191a183425c305b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-05-27 11:15:46 -0300

    feat: adiciona ferramentas e tecnologias no resumo

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -19,10 +19,27 @@ const Resume: React.FC = () => {
           <h2>o que faço atualmente?</h2>
           <h3>estou trabalhando como Analista de Desenvolvimento III em modo home office e regime CLT</h3>
           <p>tenho experiência de 5 anos na área de tecnologia.
-
-Atualmente, trabalho com desenvolvimento ReactJs, React Native, .NET e NodeJs. 
-
-Trabalhei anteriormente com C# e Análise de Sistemas, onde obtive grande experiência com POO, linguagem SQL e bancos de dados relacionais.</p>
+            Atualmente, trabalho com desenvolvimento ReactJs, React Native, .NET e NodeJs. 
+            Trabalhei anteriormente com C# e Análise de Sistemas, onde obtive grande experiência com POO, linguagem SQL e bancos de dados relacionais
+          </p>
+          <section className='tools-container'>
+            <h4>Ferramentas e tecnologias</h4>
+            <ul className='tools-content'>
+              <li>NodeJs</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>ReactJs</li>
+              <li>React Native</li>
+              <li>C#</li>
+              <li>.NET</li>
+              <li>Windows Forms</li>
+              <li>Angular</li>
+              <li>Material Design</li>
+              <li>SQL</li>
+              <li>Refatoração</li>
+              <li>Gitflow</li>
+            </ul>
+          </section>
         </section>
       </section>
 
