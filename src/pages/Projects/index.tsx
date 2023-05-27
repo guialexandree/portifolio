64d11aab19a4db0efb9dc7949a030bbbaf48002f
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-05-27 11:15:24 -0300

    feat: adiciona area de projetos

---
+++ src/pages/Projects/index.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@/components'
+import './styles.scss'
+
+const Projects: React.FC = () => {
+  return (
+    <section className='resume-container' id='projetos'>
+      <Title title='projetos' subtitle='projetos' />
+    </section>
+  )
+}
+
+export default Projects
